ce63167d76b09742809bc4a1d2dcdc9ce88e86ec
Pavel <151254599+PavelFr8@users.noreply.github.com>
2024-09-22 03:25:27 +0300

    hotfix

--- app/modules/phone_history/routes.py
+++ app/modules/phone_history/routes.py
@@ -5,21 +5,26 @@ from flask import render_template
 from flask_login import login_required, current_user
 
 from . import module
-from app import db
-from app.models import User
 
 
 # Create "Your phone" page
 @module.route('/')
 @login_required
 def history():
-    phone_history = json.loads(current_user.phone.history)
+    try:
+        phone_history = json.loads(current_user.phone.history)
 
-    phone_history = {date: events for date, events in phone_history.items() if events is not None}
+        filtered = {date: events for date, events in phone_history.items() if events is not None}
 
-    phone_history = dict(sorted(
-        phone_history.items(),
-        key=lambda item: datetime.strptime(item[0], '%Y.%m.%d') if '.' in item[0] else float('inf'),
-        reverse=True
-    ))
-    return render_template('phone/phone.html', history=phone_history)
+        filtered = dict(sorted(
+            filtered.items(),
+            key=lambda item: datetime.strptime(item[0], '%Y.%m.%d') if '.' in item[0] else float('inf'),
+            reverse=True
+        ))
+
+        if not phone_history:
+            filtered = None
+    except Exception:
+        filtered = None
+
+    return render_template('phone/phone.html', history=filtered)
